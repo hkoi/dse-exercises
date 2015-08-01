2f4c2187ae579a8ef5fd8c4b25b50a7b32008581
Tony Wong <microtony@gmail.com>
2015-08-01 15:04:14 +0800

    Finished D105

---
+++ exercises/D105/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int y1, m1, d1, y2, m2, d2;
+	scanf("%d %d %d", &y1, &m1, &d1);
+	scanf("%d %d %d", &y2, &m2, &d2);
+	int a = 10000 * y1 + 100 * m1 + d1;
+	int b = 10000 * y2 + 100 * m2 + d2;
+	if (a < b) {
+		printf("Before\n");
+	} else if (a == b) {
+		printf("Same\n");
+	} else {
+		printf("After\n");
+	}
+	return 0;
+}
