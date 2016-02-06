99df0af15741543858a5c8b7bf7fb29bc6267bb5
Tony Wong <microtony@gmail.com>
2016-02-06 18:56:45 +0800

    Fixes D301 checker

--- exercises/D301/checker.cpp
+++ exercises/D301/checker.cpp
@@ -1,5 +1,5 @@
 #include "testlib.h"
-
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char * argv[])
@@ -14,6 +14,13 @@ int main(int argc, char * argv[])
     while (!ouf.seekEof()) {
         s += ouf.readLine();
     }
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+    transform(c.begin(), c.end(), c.begin(), ::tolower);
+    transform(d.begin(), d.end(), d.begin(), ::tolower);
+    transform(e.begin(), e.end(), e.begin(), ::tolower);
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+
     ensuref(s.find(a) != -1, "A not found");
     ensuref(s.find(b) != -1, "B not found");
     ensuref(s.find(c) != -1, "C not found");
