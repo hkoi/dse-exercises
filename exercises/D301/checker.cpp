3ff1df17f1f23f1858fc648f93ecabb92d839d89
Tony Wong <microtony@gmail.com>
2020-10-06 16:50:17 +0900

    Fix D301 checker

--- exercises/D301/checker.cpp
+++ exercises/D301/checker.cpp
@@ -21,11 +21,11 @@ int main(int argc, char * argv[])
     transform(e.begin(), e.end(), e.begin(), ::tolower);
     transform(s.begin(), s.end(), s.begin(), ::tolower);
 
-    if (s.find(a) == -1) quitf_(_wa, "A not found");
-    if (s.find(b) == -1) quitf_(_wa, "B not found");
-    if (s.find(c) == -1) quitf_(_wa, "C not found");
-    if (s.find(d) == -1) quitf_(_wa, "D not found");
-    if (s.find(e) == -1) quitf_(_wa, "E not found");
+    if (s.find(a) == -1) quitf(_wa, "A not found");
+    if (s.find(b) == -1) quitf(_wa, "B not found");
+    if (s.find(c) == -1) quitf(_wa, "C not found");
+    if (s.find(d) == -1) quitf(_wa, "D not found");
+    if (s.find(e) == -1) quitf(_wa, "E not found");
     quitf(_ok, "All found");
     return 0;
 }
