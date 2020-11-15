87ec4985b2d311dcf233c99ff9c1f1d6a9f6ec14
christycty <42493801+christycty@users.noreply.github.com>
2020-11-15 12:19:14 +0800

    Update solution.pas

--- exercises/D209/solution.pas
+++ exercises/D209/solution.pas
@@ -1,6 +1,6 @@
 var
-	a: array[1..10] of real;
-	i, n: integer;
+	a: array[1..10] of longint;
+	i, n: longint;
 	ok: boolean;
 begin
 	readln(n);
@@ -8,14 +8,13 @@ begin
 		read(a[i]);
 	ok := true;
 	for i := 1 to n do
-		if (a[i] <> trunc(a[i]) 
-		or a[i] <= 100 or a[i] >= 50000) then 
+		if (a[i] <= 100) or (a[i] >= 50000) then 
 			ok := false;
 	for i := 2 to n do
 		if (a[i-1] >= a[i]) then 
 			ok := false;
 	if ok then 
-		writeln("Valid") 
+		writeln('Valid') 
 	else 
-		writeln("Invalid")
+		writeln('Invalid')
 end.
