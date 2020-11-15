4d448d42a6f89e339c41a20d17241cf9910b4c93
christycty <42493801+christycty@users.noreply.github.com>
2020-11-15 12:00:30 +0800

    Update solution.c

--- exercises/D209/solution.c
+++ exercises/D209/solution.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-float a[10];
+int a[10];
 int n, i;
 bool ok = true;
 int main() {
 	scanf("%d", &n);
 	for (i = 0; i < n; i++)
-		scanf("%f", &a[i]);
+		scanf("%d", &a[i]);
 	for (i = 0; i < n; i++)
-		if (a[i] != (int)a[i]
-		|| a[i] <= 100 || a[i] >= 50000)
+		if (a[i] <= 100 || a[i] >= 50000)
 			ok = false;
 	for (i = 1; i < n; i++)
-		if (a[i-1] <= a[i])
+		if (a[i-1] >= a[i])
 			ok = false;
 	if (ok)
 		printf("Valid");
