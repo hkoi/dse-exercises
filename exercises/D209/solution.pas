d3215afbe5a6a0d20cde8e1a5b059901bf096d91
christycty <42493801+christycty@users.noreply.github.com>
2020-12-18 13:47:11 +0800

    Update solution.pas

--- exercises/D209/solution.pas
+++ exercises/D209/solution.pas
@@ -1,18 +1,19 @@
 var
-	a: array[1..10] of longint;
-	i, n: longint;
+	i, n, t1, t2: longint;
 	ok: boolean;
 begin
-	readln(n);
-	for i := 1 to n do 
-		read(a[i]);
 	ok := true;
-	for i := 1 to n do
-		if (a[i] <= 100) or (a[i] >= 50000) then 
+	readln(n);
+	read(t1);
+	if (t1 <= 100) or (t1 >= 50000) then
+		ok := false;
+	for i := 1 to n - 1 do
+	begin
+		read(t2);
+		if (t2 <= 100) or (t2 >= 50000) or (t1 >= t2) then 
 			ok := false;
-	for i := 2 to n do
-		if (a[i-1] >= a[i]) then 
-			ok := false;
+		t1 := t2;
+	end;
 	if ok then 
 		writeln('Valid') 
 	else 
