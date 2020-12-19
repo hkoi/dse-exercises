8b3add3e216683421888fa32f9a19d70f89ed3ea
Tony Wong <microtony@gmail.com>
2020-12-19 15:20:25 +0900

    Improve D210

--- exercises/D210/solution.c
+++ exercises/D210/solution.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main() {
-	int n, x, y, x1, x2, y1, y2, ans = 0;
-	scanf("%d%d%d", &n, &x, &y);
-	x1 = x;
-	y1 = y;
-	for (int i = 2; i <= n; i++) {
-		scanf("%d%d", &x2, &y2);
-		ans += x1 * y2 - x2 * y1;
-		x1 = x2;
-		y1 = y2;
-	}
-	ans += x2 * y - x * y2;
-	printf("%.3f\n", ans / 2.0);
-	return 0;
+    int n, x, y, x1, x2, y1, y2, sum = 0;
+    scanf("%d%d%d", &n, &x, &y);
+    x1 = x;
+    y1 = y;
+    for (int i = 2; i <= n; i++) {
+        scanf("%d%d", &x2, &y2);
+        sum += x1 * y2 - x2 * y1;
+        x1 = x2;
+        y1 = y2;
+    }
+    sum += x2 * y - x * y2;
+    printf("%.1f\n", sum / 2.0);
+    return 0;
 }
--- exercises/D210/solution.pas
+++ exercises/D210/solution.pas
@@ -1,17 +1,17 @@
 var
-	i, n, x, y, x1, x2, y1, y2, ans: longint;
+    i, n, x, y, x1, x2, y1, y2, sum: longint;
 begin
-	ans := 0;
-	read(n, x, y);
-	x1 := x;
-	y1 := y;
-	for i := 2 to n do
-	begin
-		read(x2, y2);
-		ans := ans + x1 * y2 - x2 * y1;
-		x1 := x2;
-		y1 := y2;
-	end;
-	ans := ans + x2 * y - x * y2;
-	writeln(ans / 2 : 0 : 3);
+    sum := 0;
+    read(n, x, y);
+    x1 := x;
+    y1 := y;
+    for i := 2 to n do
+    begin
+        read(x2, y2);
+        sum := sum + x1 * y2 - x2 * y1;
+        x1 := x2;
+        y1 := y2;
+    end;
+    sum := sum + x2 * y - x * y2;
+    writeln(sum / 2 : 0 : 1);
 end.
