329fb9d9bd1a69e7ac23f72013b5b8d6cf0d953b
Tony Wong <microtony@gmail.com>
2020-12-19 15:26:54 +0900

    Improve D209

--- exercises/D209/solution.c
+++ exercises/D209/solution.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int n, i, t1, t2;
 bool ok = true;
 int main() {
-	scanf("%d", &n);
-	scanf("%d", &t1);
-	if (t1 <= 100 || t1 >= 50000)
-		ok = false;
-	for (i = 0; i < n - 1; i++){
-		scanf("%d", &t2);
-		if (t2 <= 100 || t2 >= 50000 || t1 >= t2)
-			ok = false;
-		t1 = t2;
-	}
-	if (ok)
-		printf("Valid");
-	else
-		printf("Invalid");
-	return 0;
+    scanf("%d", &n);
+    scanf("%d", &t1);
+    if (t1 <= 100 || t1 >= 50000)
+        ok = false;
+    for (i = 2; i <= n; i++) {
+        scanf("%d", &t2);
+        if (t2 <= 100 || t2 >= 50000 || t1 >= t2)
+            ok = false;
+        t1 = t2;
+    }
+    if (ok)
+        printf("Valid");
+    else
+        printf("Invalid");
+    return 0;
 }
--- exercises/D209/solution.pas
+++ exercises/D209/solution.pas
@@ -1,21 +1,21 @@
 var
-	i, n, t1, t2: longint;
-	ok: boolean;
+    i, n, t1, t2: longint;
+    ok: boolean;
 begin
-	ok := true;
-	readln(n);
-	read(t1);
-	if (t1 <= 100) or (t1 >= 50000) then
-		ok := false;
-	for i := 1 to n - 1 do
-	begin
-		read(t2);
-		if (t2 <= 100) or (t2 >= 50000) or (t1 >= t2) then 
-			ok := false;
-		t1 := t2;
-	end;
-	if ok then 
-		writeln('Valid') 
-	else 
-		writeln('Invalid')
+    ok := true;
+    readln(n);
+    read(t1);
+    if (t1 <= 100) or (t1 >= 50000) then
+        ok := false;
+    for i := 2 to n do
+    begin
+        read(t2);
+        if (t2 <= 100) or (t2 >= 50000) or (t1 >= t2) then 
+            ok := false;
+        t1 := t2;
+    end;
+    if ok then 
+        writeln('Valid') 
+    else 
+        writeln('Invalid')
 end.
