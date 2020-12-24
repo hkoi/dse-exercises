7e910e143b386f3d78cbb5a012008411ec138f91
christycty <42493801+christycty@users.noreply.github.com>
2020-12-24 14:56:21 +0800

    Updated D406 (#9)
    * Update test.json
    
    * Update solution.pas
    
    * Update test.json
    
    * Update zh.htm
    
    * Update zh.htm
    
    Co-authored-by: Tony Wong <microtony@gmail.com>

--- exercises/D406/solution.c
+++ exercises/D406/solution.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
 
-int a[20];
-int n, i;
+int a[20], b[20];
+int n, i, j, c;
+bool found;
 int main(){
 	scanf("%d", &n);
 	for (i = 0; i < n; i++)
 		scanf("%d", &a[i]);
-	printf("%d ", a[0]);
-	for (i = 1; i < n; i++)
-		if (a[i] != a[i-1])
-			printf("%d ", a[i]);
+	c = 0;
+	for (i = 0; i < n; i++){
+		found = 0;
+		for (j = 0; j < i; j++)
+			if (a[j] == a[i]) found = 1;	
+		if (found == 0) {
+			b[c] = a[i];
+			c++;
+		}
+	}
+	printf("%d\n", c);
+	for (i = 0; i < c; i++)
+		printf("%d ", b[i]);
 	return 0;
 }
--- exercises/D406/solution.pas
+++ exercises/D406/solution.pas
@@ -1,12 +1,24 @@
 var
-	a: array[1..20] of longint;
-	i, n: integer;
+	a, b: array[1..20] of longint;
+	i, j, n, c: integer;
+	found: boolean;
 begin
 	readln(n);
 	for i := 1 to n do 
 		read(a[i]);
-	write(a[1], ' ');
-	for i := 2 to n do
-		if (a[i] <> a[i-1]) then 
-			write(a[i], ' ')
+	c := 0;
+	for i := 1 to n do
+	begin
+		found := false;
+		for j := 1 to i - 1 do
+			if a[j] = a[i] then found := true;
+		if found = false then
+		begin
+			c := c + 1;
+			b[c] := a[i];
+		end;
+	end;
+	writeln(c);
+	for i := 1 to c do
+		write(b[i], ' ');
 end.
