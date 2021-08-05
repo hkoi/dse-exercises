0397c65eef1edd59f83104d0f08c62d0aad66138
one-four104 <susannaxchan@gmail.com>
2021-08-05 17:46:47 +0800

    Fix D103 python solution input

--- exercises/D103/solution.py
+++ exercises/D103/solution.py
@@ -1,5 +1,7 @@
 import math
 
-a, b, c = input().split()
-r = 3.141592653 * float(c) / 180.0
-print(f'{0.5 * float(a) * float(b) * math.sin(r):.3f}')
+a = float(input())
+b = float(input())
+c = float(input())
+r = 3.141592653 * c / 180.0
+print(f'{0.5 * a * b * math.sin(r):.3f}')
