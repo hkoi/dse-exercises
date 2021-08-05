b5ae600db13483463e3195c2b16ba2f6ccec5cb7
one-four104 <susannaxchan@gmail.com>
2021-08-05 18:07:00 +0800

    D104 solutions

---
+++ exercises/D104/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath> 
+#include <iomanip>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int d = b * b - 4 * a * c;
+    cout << fixed << setprecision(3);
+    if (d < 0) {
+        cout << "None" << endl;
+    } else if (d == 0) {
+        cout << -b / (2.0 * a) << endl;
+    } else {
+        double x = (-b - sqrt(d)) / (2.0 * a);
+        double y = (-b + sqrt(d)) / (2.0 * a);
+        if (x < y) {
+            cout << x << " " << y << endl;
+        } else {
+            cout << y << " " << x << endl;
+        }
+    }
+}
---
+++ exercises/D104/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = b * b - 4 * a * c;
+        if (d < 0) {
+            System.out.println("None");
+        } else if (d == 0) {
+            System.out.printf("%.3f\n", -b / (2.0 * a));
+        } else {
+            double x = (-b - Math.sqrt(d)) / (2.0 * a);
+            double y = (-b + Math.sqrt(d)) / (2.0 * a);
+            if (x < y) {
+                System.out.printf("%.3f %.3f\n", x, y);
+            } else {
+                System.out.printf("%.3f %.3f\n", y, x);
+            }
+        }
+    }
+}
