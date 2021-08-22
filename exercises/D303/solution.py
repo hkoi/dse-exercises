7e38ffd8d23c72656b00a0f8ea7f2c033c60e98c
one-four104 <susannaxchan@gmail.com>
2021-08-22 22:25:04 +0800

    Change handling of string in D303 and D304

--- exercises/D303/solution.py
+++ exercises/D303/solution.py
@@ -1,10 +1,11 @@
 s = input()
 length = len(s)
-t = s
+t = list(s)
 for i in range(length // 2):
     c = t[i]
-    t = t[:i] + t[length - i - 1] + t[i + 1:]
-    t = t[:length - i - 1] + c + t[length - i:]
+    t[i] = t[length - i - 1]
+    t[length - i - 1] = c
+t = ''.join(t)
 print(t)
 if s == t:
     print('Yes')
